0663520fde250a5baae1462c41b4163e64d8e943
Christopher Lau <christopherjnlau@gmail.com>
2018-10-23 00:01:21 -1000

    landing page mockup finished in 24 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='back'>
+        <Grid container centered stackable columns= {3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column textAlign='center'>
+            <Icon size = 'huge' inverted color ='red' name= 'users'/>
+            <Header inverted color ='red' as='h1'>Multiple Users</Header>
+            <Header inverted color ='red' as='h3'>
+              This address book enables any number of users to register and save their business contacts.
+            You can only see the contacts you have created.
+            </Header>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+          <Grid.Column textAlign='center'>
+            <Icon size = 'huge' inverted color ='red' name= 'alternate file'/>
+            <Header inverted color ='red' as='h1'>Contact Details</Header>
+            <Header inverted color ='red' as='h3'>
+              For each contact, you can save their name, address, and phone number.
+            </Header>
+          </Grid.Column>
+
+          <Grid.Column textAlign='center'>
+            <Icon size ='huge' inverted color ='red' name= 'checked calendar'/>
+            <Header inverted color ='red' as='h1'>Timestamped Notes</Header>
+            <Header inverted color ='red' as='h3'>
+              Each time you make contact with a contact, you can write a note that summarizes the conversation.
+              This note is saved alon gwith a timestamp with the contact.
+            </Header>
           </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }
