e26f728ac544c832a6b2a4b04ccaf6f9ac839d33
Christopher Lau <christopherjnlau@gmail.com>
2018-10-22 23:29:18 -1000

    landing page mockup finished in 45 minutes

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -5,8 +5,8 @@ class Footer extends React.Component {
   render() {
     const divStyle = { paddingTop: '15px' };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
+        <footer >
+          <div style={divStyle} color: 'white' className="ui center aligned container">
             <hr />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
